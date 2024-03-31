16e108af0dea220ca4caa94b0137f0cccf62cf84
“jaimejb” <jaimejb.code@gmail.com>
2024-03-31 13:32:35 -0600

    Agregue Nuevo skill SQL

--- src/skillsList/skillsList.js
+++ src/skillsList/skillsList.js
@@ -8,7 +8,7 @@ export const skillsList = [
     height="52"
     viewBox="0 0 24 24"
     stroke-width="1.5"
-    stroke=" #ebe820"
+    stroke="#ffbf00"
     fill="none"
     stroke-linecap="round"
     stroke-linejoin="round"
@@ -109,6 +109,16 @@ export const skillsList = [
     <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
   </svg>`,
     certification: "https://ude.my/UC-828ad76b-fed5-46af-89bf-42137a7f33ac",
+  },
+  {
+    name: "SQL / PostgreSQL",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sql" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff9300" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M12 8a2 2 0 0 1 2 2v4a2 2 0 1 1 -4 0v-4a2 2 0 0 1 2 -2z" />
+    <path d="M17 8v8h4" />
+    <path d="M13 15l1 1" />
+    <path d="M3 15a1 1 0 0 0 1 1h2a1 1 0 0 0 1 -1v-2a1 1 0 0 0 -1 -1h-2a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1" />
+  </svg>`,
   },
   {
     name: "MongoDB / Mongoose",
